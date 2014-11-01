da1fdea27a2f6ce3eec42edaacb86218d33e1b56
Eric Murphy <murph141@purdue.edu>
2014-11-01 19:24:23 -0400

    Small fix

--- Counter/counter.v
+++ Counter/counter.v
@@ -5,17 +5,6 @@ module counter(
   output reg [3:0] count
 );
 
-//input clock;
-//input reset;
-//input enable;
-//
-//output [3:0] count;
-//wire clock;
-//wire reset;
-//wire enable;
-//
-//reg [3:0] count;
-
 always @ (posedge clock) begin
   if(reset == 1'b1) begin
     count <= 4'b0000;
